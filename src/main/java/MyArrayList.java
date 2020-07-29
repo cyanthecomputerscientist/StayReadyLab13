296737f95b133ff254a649b8606d2efc333a58e1
Dominique Smith <cyanthecomputerscientist@gmail.com>
2020-07-29 18:28:42 -0400

    method , uml and test added

---
+++ src/main/java/MyArrayList.java
@@ -0,0 +1,112 @@
+
+public class MyArrayList <T>
+{
+    private final Integer size = 20;
+    private T[] myArray;
+    private Integer currentElement = 0; 
+
+
+    public MyArrayList()
+    {
+        myArray = (T[]) new Object[size];  
+
+    }
+    public MyArrayList(Integer s)
+    {
+        myArray = (T[]) new Object[s];
+    }
+
+    public Boolean add(T type)
+    {
+        Boolean canAdd = false;
+        if(currentElement < size)
+        {
+            canAdd = true;
+            myArray[currentElement] = type;
+            currentElement++;
+        }
+        else
+        {
+            T[] newArray = (T[]) new Object[size * 2];
+            for (int i = 0; i < myArray.length; i++) 
+            {
+                newArray[i] = myArray[i];
+            }
+            myArray[currentElement] = type;
+            currentElement++;
+            myArray = newArray;
+        }
+
+        return canAdd;
+    }
+    public Boolean remove(Integer index)
+    {
+        Boolean canRemove = true;
+        
+        return canRemove;
+    }
+    public Object get(Integer index)
+    {
+        Object got = myArray[index];
+        return got; 
+    }
+    public void set(Integer index, T element)
+    {
+        myArray[index] = element;
+    }
+    public Boolean contains(Object o)
+    {
+        Boolean doesContain = false;
+        for (int i = 0; i < myArray.length; i++) 
+        {
+            if (myArray[i] == o) 
+            {
+                doesContain = true;
+                return doesContain;   
+            }    
+        }
+        return doesContain;
+    }
+    public Boolean isEmpty()
+    {
+        Boolean empty = true;
+        if (currentElement > 0) 
+        {
+            empty = false; 
+            return empty;    
+        }
+        return empty;
+    }
+    public void clear()
+    {
+            for (int i = 0; i < myArray.length; i++) 
+            {
+                myArray[i]= null;    
+            }
+            currentElement = 0;
+    }
+    public Integer indexOf(T type)
+    {
+        Integer index = -1; 
+        for (int i = 0; i < myArray.length; i++) 
+        {
+            if(myArray[i].equals(type) == true)
+            {
+                index = i ;
+                return index;
+            }    
+        }
+        return index;
+    }
+    public Integer size()
+    {
+        Integer size = currentElement;
+        return size;
+    }
+    public T[] getArray()
+    {
+        return myArray;
+    }
+
+
+}
---
+++ src/test/java/MyArrayListTest.java
@@ -0,0 +1,151 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyArrayListTest 
+{
+
+   @Test
+    public void MyArrayListTest1()
+    {
+        MyArrayList<Object> myArrayList = new MyArrayList<>();
+        //When
+        Integer expected = 20; 
+        Integer actual = myArrayList.getArray().length;
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void MyArrayListTest2()
+    {
+        //Given
+        MyArrayList<Object> myArrayList = new MyArrayList<>(40);
+        //When
+        Integer expected = 40;
+        Integer actual = myArrayList.getArray().length;
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void addTest()
+    {
+        //Given
+        MyArrayList<Object> myArrayList = new MyArrayList<>();
+        Integer type = 10; 
+        Object[] array = myArrayList.getArray();
+        //Then 
+        myArrayList.add(type);
+        Object actual = array[0];
+        Object expected = 10;
+        //When 
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void removeTest()
+    {
+        //Given
+        MyArrayList<Object> myArrayList = new MyArrayList<>();
+        Integer type1 = 10; 
+        Integer type = 5;
+        myArrayList.add(type);
+        myArrayList.add(type1);
+        //when
+        myArrayList.remove(2);
+        Integer expected = 1; 
+        Integer actual = myArrayList.size();
+        //Then
+        Assert.assertEquals(expected, actual);
+        
+    }
+    @Test
+    public void getTest()
+    {
+        MyArrayList<Object> myArrayList = new MyArrayList<>();
+        myArrayList.add(5);
+        myArrayList.add(10);
+        //When
+        Object expected = 5; 
+        Object actual = myArrayList.get(0);
+        //Then 
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void setTest()
+    {
+        //Given
+        MyArrayList<Object> myArrayList = new MyArrayList<>();
+        myArrayList.set(0,5);
+        Object[] array = myArrayList.getArray();
+        Object actual = array[0];
+        Object expected = 5;
+        Assert.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void containsTest()
+    {
+        //Given
+        MyArrayList<Object> myArrayList = new MyArrayList<>();
+        myArrayList.add(15);
+        //When
+        Boolean actual =myArrayList.contains(15);
+        Boolean expected = true;
+        Assert.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void isEmptyTest()
+    {
+        //Given
+        MyArrayList<Object> myArrayList = new MyArrayList<>();
+        myArrayList.add(10);
+        //When 
+        Boolean actual = myArrayList.isEmpty();
+        Boolean expected = false;
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void clearTest()
+    {
+        //Given 
+        MyArrayList<Object> myArrayList = new MyArrayList<>();
+        myArrayList.add(20);
+        //When
+        myArrayList.clear();
+        Integer expected = 0; 
+        Integer actual = myArrayList.size();
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void indexOfTest()
+    {
+        //Given
+        MyArrayList<Object> myArrayList = new MyArrayList<>();
+        myArrayList.add(5);
+        myArrayList.add(10);
+        myArrayList.add(20);
+        myArrayList.add(10);
+        //When 
+        Integer actual = myArrayList.indexOf(10);
+        Integer expected = 1;
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void sizeTest()
+    {
+        //Given
+        MyArrayList<Object> myArrayList = new MyArrayList<>();
+        myArrayList.add(20);
+        myArrayList.add(10); 
+        //When 
+        Integer expected = 2;
+        Integer actual = myArrayList.size();
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+
+}
